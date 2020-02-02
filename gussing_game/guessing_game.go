2cf172bf84b47cbf018e0a72978227085cc5edb6
Sign <avery.carty7@gmail.com>
2020-02-02 09:59:41 -0500

    added printError func

--- gussing_game/guessing_game.go
+++ gussing_game/guessing_game.go
@@ -11,6 +11,13 @@ import (
 	"strconv"
 )
 
+func printError(err error ) (string) {
+		if err != nil {
+			log.Fatal(err)
+		}
+		return ""
+}
+
 func main() {
 	//////Generate Rand number between 1-100
 	seconds := time.Now().Unix()
@@ -21,23 +28,18 @@ func main() {
 	//////Capture input from user
 
 	//////Compare answer with target
-
 	fmt.Println("I've chosen a random number between 1 and 100.\nCan you guess it?")
 	success := false
 	for x := 1; x <= 10; x++ {
 		reader := bufio.NewReader(os.Stdin)
 		input, err := reader.ReadString('\n')
-		if err != nil {
-			log.Fatal(err)
-		}
+		printError(err)
 		input = strings.TrimSpace(input)
 		///////////////////////////
 
 		//////Convert input to int
 		guess, err := strconv.Atoi(input)
-		if err != nil {
-			log.Fatal(err)
-		}
+		printError(err)
 		//////////////////////////
 
 		if guess < target {
