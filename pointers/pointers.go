c0bfd70f816ab852244c11a8baa269d80bdb3fda
Sign <avery.carty7@gmail.com>
2020-02-02 11:20:37 -0500

    added comment

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -4,12 +4,19 @@ import (
 	"fmt"
 )
 
+/* this function will change the value of
+the variable named "fifty" in the main
+function directly, after passing in
+the variable "seven" which was assigned
+to the address of the variable "fifty".
+*/
 func pointerMagic(pointer *int) {
 	*pointer = 7
 }
 
 func main() {
 	fifty := 50
+
 	seven := &fifty
 	fmt.Println("Before pointerMagic")
 	fmt.Println("-------------------")
